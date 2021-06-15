644f1edcc240eeb51b47ea0b43152954107637c7
PBTO90 <bcss.90@outlook.com>
2021-06-14 20:02:24 -0500

    Videos del curso abarcados 1-23

---
+++ BlazorPeliculas/BlazorPeliculas/Client/Helpers/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class Helpers
+    {
+        public static string ConvierteEnMayusculas(string texto) 
+        {
+            return texto.ToUpper();
+        }
+    }
+}
---
+++ BlazorPeliculas/BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { set; get; }
+        public DateTime FechaLanzamiento { set; get; }
+    }
+}
