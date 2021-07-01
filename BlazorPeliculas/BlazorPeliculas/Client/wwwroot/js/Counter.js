75ba386cc7859529aced53ff4e0cbeafee03848c
PBTO90 <bcss.90@outlook.com>
2021-06-30 22:42:21 -0500

    Se continua con el curso

---
+++ BlazorPeliculas/BlazorPeliculas/Client/wwwroot/js/Counter.js
@@ -0,0 +1,4 @@
+﻿export function AlertaMensaje(mensaje)
+{
+    alert("Hola desde JavaScript, tu mensaje desde C# es : " + mensaje);
+}
---
+++ BlazorPeliculas/BlazorPeliculas/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,11 @@
+﻿function pruebaPuntoNetStatic()
+{
+    DotNet.invokeMethodAsync("BlazorPeliculas.Client", "ObtenerCurrentCount").then(resultado => {
+        console.log("conteo desde javaScript" + resultado);
+    });
+}
+
+function pruebaPuntoNetInstancia(dotnetHelper)
+{
+    dotnetHelper.invokeMethodAsync("HazmeClick");
+}
